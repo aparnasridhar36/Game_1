c391b23ddb44ccdf37b460149c0cc5a6194b51aa
Yasmin <misrayasmin@gmail.com>
2018-02-23 18:21:31 -0600

    added player file and edited start point

---
+++ player.py
@@ -0,0 +1,54 @@
+import items
+
+class Player:
+	def __init__(self):
+		self.inventory = [items.Rock(),
+						items.Dagger(),
+						items.Crusty_Bread()]
+		self.gold = 5
+		self.hp = 100
+		self.x = 3
+		self.y = 3
+
+	def print_inventory(self):
+		print("Inventory:")
+		for item in self.inventory:
+			print('* ' + str(item))
+			best_weapon = self.most_powerful_weapon()
+		print("* %i Gold" % self.gold)
+		print("Your best weapon is your {}".format(best_weapon))
+	
+	def most_powerful_weapon(self):
+		max_damage = 0
+		best_weapon = None
+		for item in self.inventory:
+			try:
+				if item.damage > max_damage:
+					best_weapon = item
+					max_damage = item.damage
+			except AttributeError:
+				pass
+		return best_weapon
+		
+	def move(self, dx, dy):
+		self.x += dx
+		self.y += dy
+
+	def move_north(self):
+		self.move(dx=0, dy=-1)
+
+	def move_south(self):
+		self.move(dx=0, dy=1)
+
+	def move_east(self):
+		self.move(dx=1, dy=0)
+
+	def move_west(self):
+		self.move(dx=-1, dy=0)
+		
+	def handle_input(self, verb, noun1, noun2):
+		if(verb == 'check'):
+			for item in self.inventory:
+				if item.name.lower() == noun1:
+					return [True, item.check_text()]
+		return [False, ""]
